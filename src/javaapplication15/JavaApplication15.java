6c418133e79243c0bc9962b5d3f53e9158917906
eyobkibret <eyobkibret05@gmail.com>
2018-05-16 12:10:47 -0700

    the third change

--- src/javaapplication15/JavaApplication15.java
+++ src/javaapplication15/JavaApplication15.java
@@ -15,6 +15,7 @@ public class JavaApplication15 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        private int x;
         // TODO code application logic here
     }
     
